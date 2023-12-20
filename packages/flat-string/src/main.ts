ebfdcb368bc111e59d6b920f572d6365eef62144
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-12-20 17:27:41 +0330

    feat(flat-string): add new package for simplifies the complex C structures that are part of a combined JavaScript string

---
+++ packages/flat-string/src/main.ts
@@ -0,0 +1,15 @@
+/**
+ * This function simplifies the complex C structures that are part of a combined JavaScript string.
+ *
+ * @param str The string to flatten.
+ * @returns The flattened string.
+ * @example
+ * ```typescript
+ * flatString(myStr);
+ * ```
+ */
+export const flatString = (str: string): string => {
+  // @ts-expect-error because it alters wrong compilation errors.
+  str | 0;
+  return str;
+};
