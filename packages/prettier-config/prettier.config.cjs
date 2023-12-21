a6fdee34591abb1d19e7ea7e431bd6624e2ea6d4
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-12-21 13:21:38 +0330

    feat(prettier-config): new package for prettier share configs

---
+++ packages/prettier-config/prettier.config.cjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 140,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  tabWidth: 2,
+};
