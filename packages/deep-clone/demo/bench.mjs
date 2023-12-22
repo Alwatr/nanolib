33d5a43fdea3da2850c6f9e259831de5bd5fac79
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-12-22 13:37:30 +0330

    chore(deep-clone) benchmark script

---
+++ packages/deep-clone/demo/bench.mjs
@@ -0,0 +1,16 @@
+import {deepClone} from '@alwatr/deep-clone';
+
+const obj1 = {};
+for (let i = 0; i < 100; i++) {
+  obj1[i] = { a: 1, b: { c: 2 } };
+}
+
+console.log('start');
+
+const startTime = performance.now();
+
+for (let i = 1; i <= 100_000; i++) {
+  const obj2 = deepClone(obj1);
+}
+
+console.log(`Execution time: ${Math.round(performance.now() - startTime)}ms`);
