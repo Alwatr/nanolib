9e9f2ee77a48cd1451a80be72d33f40cbef33a52
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-12-22 18:38:05 +0330

    feat(global-scope): Add globalScope and sharedScope_ variables

--- packages/global-scope/README.md
+++ packages/global-scope/README.md
@@ -10,6 +10,10 @@ yarn add @alwatr/global-scope
 
 ## Usage
 
+### globalScope
+
+Alternative to `globalThis` that works cross-platform.
+
 ```typescript
 import {globalScope} from '@alwatr/global-scope';
 
@@ -21,4 +25,20 @@ globalScope.alwatr = {
 globalScope.setTimeout(() => {
   console.log(globalScope.alwatr.version); // 1.0.0
 }, 1_000);
+```
+
+### sharedScope_
+
+A global variable that can be used to share state across modules without accessible publicly in the global scope.
+
+```typescript
+// module1.ts
+import {sharedScope_} from '@alwatr/global-scope';
+sharedScope_.foo = 'bar';
+```
+
+```typescript
+// module2.ts
+import {sharedScope_} from '@alwatr/global-scope';
+console.log(sharedScope_.foo); // 'bar'
 ```
--- packages/global-scope/src/main.ts
+++ packages/global-scope/src/main.ts
@@ -1,8 +1,40 @@
 /**
  * Alternative to `globalThis` that works cross-platform.
+ *
+ * @example
+ * ```typescript
+ * globalScope.alwatr = {
+ *  ...globalScope.alwatr,
+ *  foo: 'bar',
+ * }
+ * ```
  */
-export const globalScope =
+export const globalScope: typeof globalThis =
   (typeof globalThis === 'object' && globalThis) ||
-  ('object' === typeof window && window) ||
-  ('object' === typeof global && global) ||
+  (typeof window === 'object' && window) ||
+  (typeof global === 'object' && global) ||
   self;
+
+/**
+ * A global variable that can be used to share state across modules without accessible publicly in the global scope.
+ *
+ * @example
+ * ```typescript
+ * // module1.ts
+ * shareScope_.foo = 'bar';
+ *
+ * // module2.ts
+ * console.log(shareScope_.foo); // 'bar'
+ * ```
+ */
+export const sharedScope_: Record<string, unknown> = {};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __shared_scope_defined__: boolean;
+}
+
+if (globalScope.__shared_scope_defined__ !== undefined) {
+  throw new Error('global_scope_module_duplicated');
+}
+globalScope.__shared_scope_defined__ = true;
