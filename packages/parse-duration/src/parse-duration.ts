d88d8367b31ded2636f8fa8fe9a3e80c5694bce2
adltalab <info@adltalab.com>
2024-01-02 16:29:03 +0330

    feat (parse duration): new package to parse-duration

--- packages/parse-duration/src/parse-duration.ts
+++ packages/parse-duration/src/parse-duration.ts
@@ -4,42 +4,26 @@ export type DurationString = `${number}${DurationUnit}`;
 
 // Conversion factors for each duration unit to seconds
 const unitConversion = {
-  s: 1,
-  m: 6_0,
-  h: 3_600,
-  d: 86_400,
-  w: 604_800,
-  M: 2_592_000,
-  y: 31_536_000,
-  ly:31_622_400,  //Leap Year (366 days)
+  s: 1_000,
+  m: 60_000,
+  h: 3_600_000,
+  d: 86_400_000,
+  w: 604_800_000,
+  M: 2_592_000_000,
+  y: 31_536_000_000,
 };
 
 // Function to parse a duration string and convert it to a specified unit
 export const parseDuration = (duration: Duration, toUnit: DurationUnit | 'ms'='ms'):number=>{
-   // Trim and type assert the duration string
   duration = duration.trim() as DurationString;
-
-  // Extract numeric part of the duration string and Check if the numeric part is a valid number
-  const durationNumberStr = duration.substring(0,duration.lenght - 1).trimEnd(); 
-  if (!isNumber(durationNumberStr)) { // if (!isNumber(durationNumber)) {
+  const durationNumberStr = duration.substring(0, duration.length - 1).trimEnd(); // trimEnd for `10 m`
+  if (!isNumber(durationNumberStr)) {
     throw new Error(`not_a_number`);
   }
-
-  // Convert the numeric part to a number
   const durationNumber = +durationNumberStr;
-
-  /* suggest 
-  const durationNumber = parseInt(duration.substring(0,duration.lenght - 1),10);
-  if (isNan(durationNumber)) throw new Error(`not_a_number`); 
-  */
-
-  // Extract the unit part of the duration string and Check if the unit is valid
-  const durationUnit = duration.substring(duration.length - 1) as DuartionUnit;
+  const durationUnit = duration.substring(duration.length - 1) as DurationUnit;
   if (unitConversion[durationUnit] == null) {
-    throw new Error(`invalid_unit`);
+    throw new Error(`invalid_init`);
   }
-  
-  // Calculate the result in the specified unit (default is milliseconds)
-  //return (durationNumber * unitConversion[durationUnit]  ) /(toUnit ==='ms'? 1 : unitConversion[toUnit]);
-  return (durationNumber * unitConversion[durationUnit] * 1000 ) /(toUnit ==='ms'? 1 : unitConversion[toUnit]);
-} 
+  return (durationNumber * unitConversion[durationUnit]) / (toUnit === 'ms' ? 1 : unitConversion[toUnit]);
+}
