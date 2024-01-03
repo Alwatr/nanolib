77fcdf6fc8701910a7d503b7fba89fd284f36833
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-03 21:24:24 +0330

    feat(dedupe): new package

---
+++ packages/dedupe/README.md
@@ -0,0 +1,11 @@
+# Dedupe
+
+A global package manager for defining and managing package versions across different parts of applications to prevent version conflicts.
+
+## Example usage
+
+```ts
+import {definePackage} from '@alwatr/dedupe';
+
+definePackage('@alwatr/logger', '2.0.0');
+```
---
+++ packages/dedupe/src/main.ts
@@ -0,0 +1,36 @@
+/// <reference types="@alwatr/nano-build/global.d.ts" />
+
+import type {Dictionary} from '@alwatr/type-helper'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __dedupe__: true;
+}
+
+if (typeof __dedupe__ !== 'undefined') {
+  throw new Error('duplicate_dedupe');
+}
+
+export const definedPackageList: Dictionary<string> = {};
+
+/**
+ * Global define package for managing package versions to prevent version conflicts.
+ * @param packageName package name including scope. e.g. `@scope/package-name`
+ * @param version package version (optional)
+ *
+ * @example
+ * ```typescript
+ * definePackage('@scope/package-name', '2.0.1');
+ * ```
+ */
+export function definePackage (packageName: string, version = 'v?'): void {
+  if (packageName in definedPackageList) {
+    throw new Error('duplicate_package', {
+      cause: packageName,
+    });
+  }
+
+  definedPackageList[packageName] = version;
+}
+
+definePackage('@alwatr/dedupe', __package_version__);
