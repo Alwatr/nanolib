572eabb610b4e0b896bc0ecdca7300a18891ff60
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-01-03 13:46:03 +0330

    chore(wait): Update polyfill.ts with type annotations and prettier-ignore

--- packages/wait/src/polyfill.ts
+++ packages/wait/src/polyfill.ts
@@ -1,20 +1,28 @@
 import {globalScope} from '@alwatr/global-scope';
 
+import type {Dictionary} from '@alwatr/type-helper';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const win = globalScope as Record<string, any>;
+export const win = globalScope as Dictionary<any>;
 
-const requestAnimationFrameFallback = (callback: FrameRequestCallback): ReturnType<typeof setTimeout> =>
-  setTimeout(() => callback(Date.now()), 1000 / 60);
+// prettier-ignore
+const requestAnimationFrameFallback =
+  (callback: FrameRequestCallback): ReturnType<typeof setTimeout> =>
+    setTimeout(() => callback(Date.now()), 1000 / 60);
 
+// prettier-ignore
 export const requestAnimationFrame: typeof globalScope.requestAnimationFrame =
   win.requestAnimationFrame ||
   win.webkitRequestAnimationFrame ||
   win.mozRequestAnimationFrame ||
   requestAnimationFrameFallback;
 
-const requestIdleCallbackFallback = (callback: () => void, options?: IdleRequestOptions): ReturnType<typeof setTimeout> =>
-  setTimeout(callback, options?.timeout ?? 2000);
+// prettier-ignore
+const requestIdleCallbackFallback =
+  (callback: () => void, options?: IdleRequestOptions): ReturnType<typeof setTimeout> =>
+    setTimeout(callback, options?.timeout ?? 2000);
 
+// prettier-ignore
 export const requestIdleCallback: typeof globalScope.requestIdleCallback =
   win.requestIdleCallback ||
   win.webkitRequestIdleCallback ||
