a39590b41f1936ec7a4abad92b7c0fdc95a99e8d
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-01-08 17:40:39 +0330

    refactor(node-fs): rewrite makeEmptyFile

--- packages/node-fs/src/make-file.ts
+++ packages/node-fs/src/make-file.ts
@@ -1,4 +1,6 @@
-import {open, close} from 'node:fs';
+import {open} from 'node:fs/promises';
+
+import { logger } from './logger.js';
 
 /**
  * Make empty file.
@@ -11,13 +13,6 @@ import {open, close} from 'node:fs';
  * ```
  */
 export async function makeEmptyFile(path: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    open(path, 'w', (err, fileDescriptor) => {
-      if (err != null) return reject(err);
-      close(fileDescriptor, (err) => {
-        if (err != null) return reject(err);
-        resolve();
-      });
-    });
-  });
+  logger.logMethodArgs?.('makeEmptyFile', '...' + path.slice(-32));
+  return (await open(path, 'w')).close();
 }
