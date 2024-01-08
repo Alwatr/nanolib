581d4f958ccb262c13f23881151616b7ec5e93ee
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-01-08 20:07:48 +0330

    feat(node-fs): writeJson method

---
+++ packages/node-fs/src/write-json.ts
@@ -0,0 +1,59 @@
+import {flatString} from '@alwatr/flat-string';
+
+import {jsonStringify} from './json';
+import {logger} from './logger';
+import {writeFile, writeFileSync} from './write-file.js';
+
+import type {MaybePromise} from '@alwatr/type-helper';
+
+/**
+ * Enhanced write json file (Asynchronous).
+ *
+ * @param path - file path
+ * @param data - json object
+ * @example
+ * ```typescript
+ * await writeJsonFile('./file.json', { a:1, b:2, c:3 });
+ * ```
+ */
+export function writeJson(path: string, data: unknown, sync?: false): Promise<void>;
+/**
+ * Enhanced write json file (Synchronous).
+ *
+ * @param path - file path
+ * @param data - json object
+ * @param sync - sync mode
+ * @example
+ * ```typescript
+ * writeJsonFile('./file.json', { a:1, b:2, c:3 }, true);
+ * ```
+ */
+export function writeJson(path: string, data: unknown, sync: true): void;
+/**
+ * Enhanced write json file.
+ *
+ * @param path - file path
+ * @param data - json object
+ * @param sync - sync mode
+ * @example
+ * ```typescript
+ * await writeJsonFile('./file.json', { a:1, b:2, c:3 }, sync);
+ * ```
+ */
+export function writeJson(path: string, data: unknown, sync: boolean): MaybePromise<void>;
+/**
+ * Enhanced write json file.
+ *
+ * @param path - file path
+ * @param data - json object
+ * @param sync - sync mode
+ * @example
+ * ```typescript
+ * await writeJsonFile('./file.json', { a:1, b:2, c:3 });
+ * ```
+ */
+export function writeJson(path: string, data: unknown, sync = false): MaybePromise<void> {
+  logger.logMethodArgs?.('writeJson', '...' + path.slice(-32));
+  const content = flatString(jsonStringify(data));
+  return sync === true ? writeFileSync(path, content) : writeFile(path, content);
+}
