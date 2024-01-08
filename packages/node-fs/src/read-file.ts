de2cf5d7110646230019c53a458092617be71922
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-01-08 20:29:22 +0330

    refactor(node-fs): separate read file functions

--- packages/node-fs/src/read-file.ts
+++ packages/node-fs/src/read-file.ts
@@ -4,74 +4,47 @@ import {readFile as readFile_} from 'node:fs/promises';
 import {flatString} from '@alwatr/flat-string';
 
 import {logger} from './common';
-import {parseJson} from './json';
-
-import type {MaybePromise} from '@alwatr/type-helper';
 
 /**
- * Enhanced read file (async).
+ * Enhanced read File (Synchronous).
  *
  * @param path - file path
  * @returns file content
  * @example
  * ```typescript
- * const fileContent = await readFile('./file.txt');
+ * const fileContent = readFileSync('./file.txt', sync);
  * ```
  */
-export function readFile(path: string): Promise<string>;
+export function readFileSync(path: string): string {
+  logger.logMethodArgs?.('readFileSync', '...' + path.slice(-32));
+  // if (!existsSync(path)) throw new Error('file_not_found');
+  try {
+    return flatString(readFileSync_(path, {encoding: 'utf-8', flag: 'r'}));
+  }
+  catch (err) {
+    logger.error('readFileSync', 'read_file_failed', {path}, err);
+    throw new Error('read_file_failed', {cause: (err as Error).cause});
+  }
+}
+
 /**
- * Enhanced read file (sync).
+ * Enhanced read File (Asynchronous).
  *
  * @param path - file path
- * @param sync - sync mode
- * @returns file content
- * @example
- * ```typescript
- * const fileContent = readFile('./file.txt', true);
- * ```
- */
-export function readFile(path: string, sync: true): string;
-/**
- * Enhanced read File.
- *
- * @param path - file path
- * @param sync - sync mode
  * @returns file content
  * @example
  * ```typescript
  * const fileContent = await readFile('./file.txt', sync);
  * ```
  */
-export function readFile(path: string, sync: boolean): MaybePromise<string>;
-/**
- * Enhanced read File.
- *
- * @param path - file path
- * @param sync - sync mode
- * @returns file content
- * @example
- * ```typescript
- * const fileContent = await readFile('./file.txt');
- * ```
- */
-export function readFile(path: string, sync = false): MaybePromise<string> {
-  logger.logMethodArgs?.('readFile', {path: path.slice(-32), sync});
+export async function readFile(path: string): Promise<string> {
+  logger.logMethodArgs?.('readFile', '...' + path.slice(-32));
   // if (!existsSync(path)) throw new Error('file_not_found');
-  if (sync === true) {
-    try {
-      return flatString(readFileSync_(path, {encoding: 'utf-8', flag: 'r'}));
-    }
-    catch (err) {
-      logger.error('readFile', 'read_file_failed', err);
-      throw new Error('read_file_failed', {cause: (err as Error).cause});
-    }
+  try {
+    return flatString(await readFile_(path, {encoding: 'utf-8', flag: 'r'}));
   }
-  // else, async mode
-  return readFile_(path, {encoding: 'utf-8', flag: 'r'}).then(
-    (content) => flatString(content),
-    (err) => {
-      logger.error('readFile', 'read_file_failed', err);
-      throw new Error('read_file_failed', {cause: (err as Error).cause});
-    },
-  );
+  catch (err) {
+    logger.error('readFile', 'read_file_failed', {path}, err);
+    throw new Error('read_file_failed', {cause: (err as Error).cause});
+  }
 }
