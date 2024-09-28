207af08d5632ed5b435428904cd5cafcf8b12cb2
Mohammad Honarvar <honarvar.info@gmail.com>
2024-09-28 22:49:08 +0330

    refactor(package-tracer): add some new `function`s & improve the code style

--- packages/package-tracer/src/main.ts
+++ packages/package-tracer/src/main.ts
@@ -1,1 +1,27 @@
-export * from './package-tracer.js';
+import {createLogger} from '@alwatr/logger';
+
+const logger = createLogger(__package_name__);
+
+import type {Dictionary} from '@alwatr/type-helper';
+
+export const packageTracer =  {
+  list: {} as Readonly<Dictionary<string[]>>,
+
+  add(packageName: string, version: string): void {
+    logger.logMethodArgs?.('add', {packageName, version});
+    (this.list[packageName] as string[]) ??= [];
+    (this.list[packageName] as string[]).push(version);
+  },
+
+  has(packageName: string): boolean {
+    const exist = Object.hasOwn(this.list, packageName);
+    logger.logMethodFull?.('has', {packageName}, exist);
+    return exist;
+  },
+
+  get(packageName: string): readonly string[] | undefined {
+    logger.logMethodArgs?.('get', {packageName});
+    return this.list[packageName];
+  },
+} as const;
+
