496a338a6366aa12dc32bbed07a3e14bd5315bd2
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-28 22:03:21 +0330

    refactor(node-fs): remove unused import from common module

--- packages/node-fs/src/common.ts
+++ packages/node-fs/src/common.ts
@@ -1,8 +1,6 @@
 import {AsyncQueue} from '@alwatr/async-queue';
 import {createLogger} from '@alwatr/logger';
 
-import type {} from '@alwatr/nano-build';
-
 export const logger = createLogger('@alwatr/node-fs');
 
 export const asyncQueue = new AsyncQueue();
