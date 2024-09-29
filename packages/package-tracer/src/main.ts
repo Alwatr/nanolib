5be7132d2275e028cc88155d32960e05da22726f
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-29 09:11:42 +0330

    refactor(package-tracer): Improve package tracking functionality
    - Refactor the `packageTracer` module to improve package tracking functionality.
    - Add new functions for adding packages and checking if a package exists.
    - Update the code style for better readability.

--- packages/package-tracer/src/main.ts
+++ packages/package-tracer/src/main.ts
@@ -1,26 +1,81 @@
-import {createLogger} from '@alwatr/logger';
-
-const logger = createLogger(__package_name__);
-
 import type {Dictionary} from '@alwatr/type-helper';
 
-export const packageTracer =  {
+/**
+ * A module for tracking package information.
+ * This module is useful for keeping track of loaded packages and their versions.
+ * It does not prevent the same package from being added multiple times!
+ * For that, you can use the `@alwatr/dedupe` package.
+ *
+ * @example
+ * ```typescript
+ * import { packageTracer } from './package-tracer';
+ *
+ * packageTracer.add('express', '4.18.2');
+ * packageTracer.add('lodash', '4.17.21');
+ *
+ * if (packageTracer.has('express')) {
+ *   const versions = packageTracer.get('express');
+ *   console.log('Express versions:', versions);
+ * }
+ * ```
+ */
+export const packageTracer = {
+  /**
+   * A dictionary storing package names and their corresponding versions.
+   */
   list: {} as Readonly<Dictionary<readonly string[]>>,
 
+  /**
+   * Adds a package and its version to the tracker.
+   * It does not prevent the same package from being added multiple times!
+   * For that, you can use the `@alwatr/dedupe` package.
+   *
+   * @param packageName - The name of the package.
+   * @param version - The version of the package.
+   *
+   * @example
+   * ```typescript
+   * packageTracer.add(__package_name__, __package_version__);
+   * ```
+   */
   add(packageName: string, version: string): void {
-    logger.logMethodArgs?.('add', {packageName, version});
     (this.list[packageName] as string[]) ??= [];
     (this.list[packageName] as string[]).push(version);
   },
 
+  /**
+   * Checks if a package exists in the tracker.
+   *
+   * @param packageName - The name of the package.
+   * @returns `true` if the package exists, `false` otherwise.
+   *
+   * @example
+   * ```typescript
+   * if (packageTracer.has('axios')) {
+   *   console.log('Axios is tracked!');
+   * }
+   * ```
+   */
   has(packageName: string): boolean {
-    const exist = Object.hasOwn(this.list, packageName);
-    logger.logMethodFull?.('has', {packageName}, exist);
+    const exist = Object.prototype.hasOwnProperty.call(this.list, packageName);
     return exist;
   },
 
+  /**
+   * Retrieves the versions of a package.
+   *
+   * @param packageName - The name of the package.
+   * @returns An array of versions or `undefined` if the package doesn't exist.
+   *
+   * @example
+   * ```typescript
+   * const reactVersions = packageTracer.get('react');
+   * if (reactVersions) {
+   *   console.log('React versions:', reactVersions);
+   * }
+   * ```
+   */
   get(packageName: string): readonly string[] | undefined {
-    logger.logMethodArgs?.('get', {packageName});
     return this.list[packageName];
   },
 } as const;
