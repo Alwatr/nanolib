db46c4844db360735271f0d4c0d6417207589aaf
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-11 20:41:20 +0330

    fix(platform-info): Update platformInfo logic and remove redundant code for prevent side-effects

--- packages/platform-info/src/main.ts
+++ packages/platform-info/src/main.ts
@@ -52,59 +52,62 @@ interface PlatformInfo {
   readonly isElectron: boolean;
 }
 
-/**
+export const platformInfo: PlatformInfo = /* #__PURE__ */ (() => {
+
+  /**
  * Represents information about the current platform.
  */
-const platformInfo_: Mutable<PlatformInfo> = {
-  development: false,
-  isNode: false,
-  isBrowser: false,
-  isWebWorker: false,
-  isDeno: false,
-  isBun: false,
-  isCli: false,
-  isNw: false,
-  isElectron: false,
-};
+  const platformInfo_: Mutable<PlatformInfo> = {
+    development: false,
+    isNode: false,
+    isBrowser: false,
+    isWebWorker: false,
+    isDeno: false,
+    isBun: false,
+    isCli: false,
+    isNw: false,
+    isElectron: false,
+  };
 
-if (typeof window === 'object' && typeof document === 'object' && document.nodeType === Node.DOCUMENT_NODE) {
-  platformInfo_.isBrowser = true;
-  // @ts-expect-error - Cannot find name 'WorkerGlobalScope'
-  platformInfo_.isWebWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope;
-}
-else if (typeof process === 'object') {
-  platformInfo_.isCli = true;
+  if (typeof window === 'object' && typeof document === 'object' && document.nodeType === Node.DOCUMENT_NODE) {
+    platformInfo_.isBrowser = true;
+    // @ts-expect-error - Cannot find name 'WorkerGlobalScope'
+    platformInfo_.isWebWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope;
+  }
+  else if (typeof process === 'object') {
+    platformInfo_.isCli = true;
 
-  if (process.versions?.node != null) {
-    platformInfo_.isNode = true;
+    if (process.versions?.node != null) {
+      platformInfo_.isNode = true;
+    }
+
+    // @ts-expect-error - Cannot find name 'Bun'
+    if (typeof Bun !== 'undefined') {
+      platformInfo_.isBun = true;
+    }
+    else if (process.versions?.electron != null) {
+      platformInfo_.isElectron = true;
+    }
+    // @ts-expect-error - Cannot find name 'nw'
+    else if (typeof nw !== 'undefined') {
+      platformInfo_.isNw = true;
+    }
   }
 
-  // @ts-expect-error - Cannot find name 'Bun'
-  if (typeof Bun !== 'undefined') {
-    platformInfo_.isBun = true;
+  // other platforms
+  // @ts-expect-error - Cannot find name 'Deno'
+  if (typeof Deno !== 'undefined') {
+    platformInfo_.isCli = true;
+    platformInfo_.isDeno = true;
   }
-  else if (process.versions?.electron != null) {
-    platformInfo_.isElectron = true;
+
+  // development
+  if (platformInfo_.isBrowser === true) {
+    platformInfo_.development = location.hostname === 'localhost' || location.hostname.indexOf('127.') === 0;
   }
-  // @ts-expect-error - Cannot find name 'nw'
-  else if (typeof nw !== 'undefined') {
-    platformInfo_.isNw = true;
+  else if (platformInfo_.isCli === true) {
+    platformInfo_.development = process.env.NODE_ENV !== 'production';
   }
-}
 
-// other platforms
-// @ts-expect-error - Cannot find name 'Deno'
-if (typeof Deno !== 'undefined') {
-  platformInfo_.isCli = true;
-  platformInfo_.isDeno = true;
-}
-
-// development
-if (platformInfo_.isBrowser === true) {
-  platformInfo_.development = location.hostname === 'localhost' || location.hostname.indexOf('127.') === 0;
-}
-else if (platformInfo_.isCli === true) {
-  platformInfo_.development = process.env.NODE_ENV !== 'production';
-}
-
-export const platformInfo: PlatformInfo = platformInfo_;
+  return platformInfo_;
+})();
