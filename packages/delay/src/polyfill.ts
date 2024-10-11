f92eeed7d917f6eb3ca9a407fab0b1ea77adc1d4
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-11 21:01:31 +0330

    refactor(delay): prevent side-effects

--- packages/delay/src/polyfill.ts
+++ packages/delay/src/polyfill.ts
@@ -7,21 +7,13 @@ const requestAnimationFrameFallback =
   (callback: FrameRequestCallback): ReturnType<typeof setTimeout> =>
     setTimeout(() => callback(Date.now()), 1000 / 60);
 
-// prettier-ignore
-export const requestAnimationFrame: GlobalThis['requestAnimationFrame'] =
-  win.requestAnimationFrame ||
-  win.webkitRequestAnimationFrame ||
-  win.mozRequestAnimationFrame ||
-  requestAnimationFrameFallback;
+export const requestAnimationFrame: GlobalThis['requestAnimationFrame'] = /* #__PURE__ */ (() =>
+  win.requestAnimationFrame || win.webkitRequestAnimationFrame || win.mozRequestAnimationFrame || requestAnimationFrameFallback)();
 
 // prettier-ignore
 const requestIdleCallbackFallback =
   (callback: () => void, options?: IdleRequestOptions): ReturnType<typeof setTimeout> =>
     setTimeout(callback, options?.timeout ?? 2000);
 
-// prettier-ignore
-export const requestIdleCallback: GlobalThis['requestIdleCallback'] =
-  win.requestIdleCallback ||
-  win.webkitRequestIdleCallback ||
-  win.mozRequestIdleCallback ||
-  requestIdleCallbackFallback;
+export const requestIdleCallback: GlobalThis['requestIdleCallback'] = /* #__PURE__ */ (() =>
+  win.requestIdleCallback || win.webkitRequestIdleCallback || win.mozRequestIdleCallback || requestIdleCallbackFallback)();
